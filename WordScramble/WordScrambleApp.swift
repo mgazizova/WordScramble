9c7f272994536d01a6d3b0de7bf0cebb18f7d608
Mariia Gazizova <mgazizova5@gmail.com>
2024-10-14 09:30:46 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Мария Газизова on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
